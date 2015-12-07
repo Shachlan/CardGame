2ee7da8906b14c91a54576ef4f6d2e2a7d116324
Shachar Langbeheim <Shachar@lightrickss-imac.local>
2015-12-07 09:55:21 +0200

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Shachar Langbeheim on 07/12/2015.
+//  Copyright © 2015 Shachar Langbeheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Shachar Langbeheim on 07/12/2015.
+//  Copyright © 2015 Shachar Langbeheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
